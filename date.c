efafe4bb4655762a7607bfef4e5e692c5257ef93
03cherry03 <03cherry03@soongsil.ac.kr>
2022-12-11 00:23:23 +0900

    diary project

--- date.c
+++ date.c
@@ -193,3 +193,24 @@ void Star_PR(int year, int month, int day) {
         else //여기에서는 안해도 상관없는 사족이기도하다;
                 printf(" ");
 }
+//입력한 날의 일기를 읽어 출력하는 함수
+void Rto_thatday(int year, int month, int day) {
+        FILE *fp;
+        char thatday[100];
+        int et, open;
+
+        sprintf(thatday, "%d%d%d.txt", year, month, day);
+        et = access(thatday, F_OK) + 1;
+
+        if( et == 1 ) { //파일이 있으면 읽기 모드로 파일을 열어 출력한다
+                printf(" %4d년%2d월%2d일 \n", year, month, day);
+                fp = fopen(thatday, "r");
+                while( ( open=getc(fp) ) != EOF )
+                        putchar(open);
+                fclose(fp);
+                printf(" *========================================================================* \n");
+        }
+        if( et == 0 ) //파일이 없으면 아래와 같은 메세지를 출력후 아무것도 열지 않고 끝낸다
+                printf("File not existence!\n");
+
+}
