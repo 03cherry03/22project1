4786115490dfbaa5e3f28a9c1d67eaaa9d95068b
03cherry03 <03cherry03@soongsil.ac.kr>
2022-12-11 23:31:31 +0900

    diary_main

---
+++ diary_Feb_PM.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include<time.h>
+#include<unistd.h>
+
+int lastday[12] = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+
+void Feb_PM(int year) {
+        if( (year % 400 == 0) || ((year % 4 == 0) && (year % 100 !=0)) )
+        //400년의 배수는 무조건 윤년임을 주의(단축평가)
+                lastday[1] = 29; //윤년일 경우에 전역변수배열 2월의 날짜를 29로 변경경시킨다
+        else
+                lastday[1] = 28;
+}
---
+++ diary_Rto_thatday.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include<time.h>
+#include<unistd.h>
+
+void Rto_thatday(int year, int month, int day) {
+        FILE *fp;
+        char thatday[100];
+        int et, open;
+
+        sprintf(thatday, "%d%d%d.txt", year, month, day);
+        et = access(thatday, F_OK) + 1;
+
+        if( et == 1 ) { //파일이 있으면 읽기 모드로 파일을 열어 출력한다
+                printf(" %4d년%2d월%2d일 \n", year, month, day);
+                fp = fopen(thatday, "r");
+                while( ( open=getc(fp) ) != EOF )
+                        putchar(open);
+                fclose(fp);
+                printf(" *========================================================================* \n");
+        }
+        if( et == 0 ) //파일이 없으면 아래와 같은 메세지를 출력후 아무것도 열지 않고 끝낸다
+                printf("File not existence!\n");
+
+}
